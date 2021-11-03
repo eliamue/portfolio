4ba540899dfd4cb4cb85ab17dd6741230e44a582
Elise Muellerleile <eliamue@live.com>
2021-11-03 04:02:04 -0500

    idklol

--- src/components/Education.js
+++ src/components/Education.js
@@ -6,16 +6,15 @@ import {
 import "react-vertical-timeline-component/style.min.css";
 import Badge from "react-bootstrap/Badge";
 import SchoolIcon from "@material-ui/icons/School";
-import WorkIcon from "@material-ui/icons/Work";
-import Education from "./Education";
 
-class Experience extends Component {
+
+class Education extends Component {
   render() {
-    if (this.props.resumeExperience && this.props.resumeBasicInfo) {
-      var sectionName = this.props.resumeBasicInfo.section_name.experience;
-      var work = this.props.resumeExperience.map(function (work, i) {
-        const technologies = work.technologies;
-        const mainTechnologies = work.mainTech;
+    if (this.props.eduExperience && this.props.resumeBasicInfo) {
+      var sectionName = this.props.resumeBasicInfo.section_name.experience.education;
+      var school = this.props.eduExperience.map(function (school, i) {
+        const technologies = school.technologies;
+        const mainTechnologies = school.mainTech;
 
         var mainTech = mainTechnologies.map((technology, i) => {
           return (
@@ -34,14 +33,14 @@ class Experience extends Component {
 
         return (
           <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            date={work.years}
+            className="vertical-timeline-element--school"
+            date={school.years}
             iconStyle={{
               background: "#AE944F",
               color: "#fff",
               textAlign: "center",
             }}
-            icon={<WorkIcon />}
+            icon={<SchoolIcon />}
             key={i}
           >
             <div style={{ textAlign: "left", marginBottom: "4px" }}>
@@ -52,13 +51,13 @@ class Experience extends Component {
               className="vertical-timeline-element-title"
               style={{ textAlign: "left" }}
             >
-              {work.title}
+              {school.title}
             </h3>
             <h4
               className="vertical-timeline-element-subtitle"
               style={{ textAlign: "left" }}
             >
-              {work.company}
+              {school.company}
             </h4>
             <div style={{ textAlign: "left", marginTop: "15px" }}>{tech}</div>
           </VerticalTimelineElement>
@@ -78,40 +77,13 @@ class Experience extends Component {
           </div>
         </div>
         <div className="col-md-8 mx-auto">
-          <VerticalTimelineElement
-            iconStyle={{
-              background: "#AE944F",
-              color: "#fff",
-              textAlign: "center",
-            }}
-            company="Perpich Center for Arts Education"
-            title="High School Diploma in Visual Arts"
-            years="09.2006 - 06.2008"
-            mainTech="[Education]"
-            technologies="[Oil Painting, Graphic Design, Printmaking]"
-            icon={<SchoolIcon />}
-          />
-          <VerticalTimeline
-            iconStyle={{
-              background: "#AE944F",
-              color: "#fff",
-              textAlign: "center",
-            }}
-            icon={<SchoolIcon />}
-          >
-            <Education 
-            company="Perpich"
-            title="pooper"
-            years="5ever"
-            mainTech="butt"
-            technologies="butt"
-            />
+          <VerticalTimeline>
+            {school}
           </VerticalTimeline>
-          <VerticalTimeline>{work}</VerticalTimeline>
         </div>
       </section>
     );
   }
 }
 
-export default Experience;
+export default Education;
